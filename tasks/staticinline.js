0f3f84e359274702d6ac108096ee58e5ba9c6d75
Maxwell Da Silva <dayvson@gmail.com>
2013-08-30 09:48:00 -0400

    implementing basepath support for static

--- tasks/staticinline.js
+++ tasks/staticinline.js
@@ -11,10 +11,16 @@ module.exports = function(grunt) {
   var datauri = require('datauri');
   var path = require('path');
   var description = 'A grunt plugin to replace url from static files such as img,js,css an put inline in a template';  
-  var resolveFilePath = function(templatePath, src){
+  var resolveFilePath = function(templatePath, src, basepath){
     var srcPath;
     if(!grunt.file.isPathAbsolute(src)){
+      if( basepath ){}
       srcPath = path.resolve(path.dirname(templatePath), src);
+      if(grunt.file.exists(srcPath)){
+        return srcPath;
+      }
+    }else if(grunt.file.isPathAbsolute(src) && basepath){
+      srcPath = path.resolve(basepath + src);
       if(grunt.file.exists(srcPath)){
         return srcPath;
       }
@@ -22,9 +28,9 @@ module.exports = function(grunt) {
     return srcPath;
   };
 
-  var baseTAGReplace = function(templatePath, tag, src){
+  var baseTAGReplace = function(templatePath, tag, src, basepath){
     var result = '';
-    var srcPath = resolveFilePath(templatePath, src);
+    var srcPath = resolveFilePath(templatePath, src, basepath);
     if(srcPath){
       result = '<TAG>\nCONTENT\n</TAG>'
                  .replace(/CONTENT/g, grunt.file.read(srcPath))
@@ -33,21 +39,21 @@ module.exports = function(grunt) {
     return result;
   };
 
-  var findReplaceScript = function(templatePath, content){
+  var findReplaceScript = function(templatePath, content, basepath){
     return content.replace(/<script.*src=['"]([^'"]+)['"].*inline=['"]true['"].*><\/script>/g, function(match, src){
-      return baseTAGReplace(templatePath, "script", src);
+      return baseTAGReplace(templatePath, "script", src, basepath);
     });
   };
   
-  var findReplaceLink = function(templatePath, content){
+  var findReplaceLink = function(templatePath, content, basepath){
     return content.replace(/<link.*href=['"]([^'"]+)['"].*inline=['"]true['"].*\/?\s*>/g, function(match, src){
-      return baseTAGReplace(templatePath, "style", src);
+      return baseTAGReplace(templatePath, "style", src, basepath);
     });
   };
 
-  var findReplaceImg = function(templatePath, content){
+  var findReplaceImg = function(templatePath, content, basepath){
     return content.replace(/<img.*src=['"]([^'"]+)['"].*inline=['"]true['"].*\/?\s*>/g, function(match, src){
-        var srcPath = resolveFilePath(templatePath, src);
+        var srcPath = resolveFilePath(templatePath, src, basepath);
         if(srcPath){
           return match.replace(/inline=['"]true['"]/g, '').replace(src, datauri(srcPath));
         }
@@ -67,7 +73,7 @@ module.exports = function(grunt) {
     return findReplaceVariables(options,
            findReplaceImg(templatePath, 
            findReplaceScript(templatePath,  
-           findReplaceLink(templatePath, content))));
+           findReplaceLink(templatePath, content, options.basepath), options.basepath), options.basepath));
   };
 
   var staticInlineTask = function(){
